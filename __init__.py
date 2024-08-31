b90eec8222cb5ce4676e329079f0877835264823
ayoub abdessadak <ayoubentje@hotmail.com>
2024-08-31 12:13:25 +0200

    random first name can be retrieved

--- __init__.py
+++ __init__.py
@@ -1,1 +1,20 @@
-import random, string
+import random, string, json
+
+class WorldNames:
+
+    def __init__(self, file: str = '/Users/ayoub/PycharmProjects/HBO-ICT/worldnames/worldnames.json'):
+        __file = open(file)
+        names_in_json = __file.readlines()[0]
+        self.names = json.loads(names_in_json)
+        self.min, self.max = 0, len(self.names)-1
+
+    def first_name(self):
+        random.shuffle(self.names)
+        return self.names[random.randint(self.min, self.max)]
+
+    def last_name(self):
+        pass
+
+world_names = WorldNames()
+first_name = world_names.first_name
+last_name = world_names.last_name
