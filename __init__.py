f8de28d3971df0c714620705b1e8e8885d09ef64
ayoub abdessadak <ayoubentje@hotmail.com>
2024-08-31 12:25:32 +0200

    Random fullname, firstname and lastname can be retrieved

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import random, string, json
 
+letters = [letter for letter in string.ascii_lowercase]
 class WorldNames:
 
     def __init__(self, file: str = '/Users/ayoub/PycharmProjects/HBO-ICT/worldnames/worldnames.json'):
@@ -8,13 +9,20 @@ class WorldNames:
         self.names = json.loads(names_in_json)
         self.min, self.max = 0, len(self.names)-1
 
+    def full_name(self):
+        return f"{self.first_name()} {self.last_name()}"
+
     def first_name(self):
         random.shuffle(self.names)
         return self.names[random.randint(self.min, self.max)]
 
     def last_name(self):
-        pass
+        _max = random.randint(3, 12)
+        random.shuffle(letters)
+        _last_name = "".join(letters[0:_max])
+        return f"{_last_name[0].upper()}{_last_name[1::].lower()}"
 
 world_names = WorldNames()
+full_name = world_names.full_name
 first_name = world_names.first_name
 last_name = world_names.last_name
